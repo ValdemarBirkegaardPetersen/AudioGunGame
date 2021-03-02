93c6f2d99cd4cf428e2fe4ac04979e57ebc6d888
EmilAceMacko <EmilAceMacko@users.noreply.github.com>
2021-03-02 09:38:02 +0100

    Project declaration
    Added Sketch.java

---
+++ src/Sketch.java
@@ -0,0 +1,21 @@
+import processing.core.PApplet;
+
+public class Sketch extends PApplet {
+    public static void main(String[] args) {
+        PApplet.main("Sketch", args);
+    }
+
+    public void settings() {
+        // Initial app settings:
+        size(256, 256);
+    }
+
+    public void setup() {
+        // Game setup:
+        background(0);
+    }
+
+    public void draw() {
+        // Game loop:
+    }
+}
